d528807485542b27a3ad851379389ddac0046b89
Corey Johnson <probablycorey@gmail.com>
2009-10-23 14:10:41 -0700

    merged the wrong git

--- sandbox.rb
+++
@@ -1,8 +0,0 @@
-snippet = ""
-
-"hello:corey:dale:johnson:".scan(/([^\:]+):/).each_with_index do |var, index|
-  name = (index == 0) ? "first" : var.first
-  snippet += "${#{index}:#{name}}"
-end
-
-puts snippet
