addf874854cb461c27295b72f7e3d8bfebcdccd3
Aman Gupta <aman@tmm1.net>
2010-02-10 01:31:32 -0800

    Add yajl based backend for wax.json.parse

--- lib/extensions/json/wax_json.c
+++ lib/extensions/json/wax_json.c
@@ -25,6 +25,163 @@ int luaopen_wax_json(lua_State *L) {
     return 1;
 }
 
+#define USE_YAJL 1
+#ifdef USE_YAJL
+
+#include "yajl/yajl_parse.h"
+#include "yajl/yajl_lex.h"
+#include "yajl/yajl_parser.h"
+#include "yajl/yajl_bytestack.h"
+#include <string.h>
+
+static yajl_handle hand;
+static yajl_bytestack state;
+enum {
+    state_begin,
+    state_array,
+    state_hash
+};
+
+void push_hash_or_array(lua_State *L) {
+    switch (yajl_bs_current(state)) {
+        case state_hash:
+            lua_rawset(L, -3);
+            break;
+        case state_array:
+            lua_rawseti(L, -2, lua_objlen(L, -2) + 1);
+            break;
+    }
+}
+
+static int push_null(void *ctx) {
+    lua_State *L = (lua_State *) ctx;
+    lua_pushnil(L);
+    push_hash_or_array(L);
+    return 1;
+}
+
+static int push_boolean(void *ctx, int boolean) {
+    lua_State *L = (lua_State *) ctx;
+    lua_pushboolean(L, boolean ? true : false);
+    push_hash_or_array(L);
+    return 1;
+}
+
+static int push_integer(void *ctx, long num) {
+    lua_State *L = (lua_State *) ctx;
+    lua_pushinteger(L, num);
+    push_hash_or_array(L);
+    return 1;
+}
+
+static int push_double(void *ctx, double num) {
+    lua_State *L = (lua_State *) ctx;
+    lua_pushnumber(L, num);
+    push_hash_or_array(L);
+    return 1;
+}
+
+static int push_string(void *ctx, const unsigned char *string, unsigned int len) {
+    lua_State *L = (lua_State *) ctx;
+    lua_pushlstring(L, (const char *)string, len);
+    push_hash_or_array(L);
+    return 1;
+}
+
+static int push_start_map(void *ctx) {
+    lua_State *L = (lua_State *) ctx;
+    yajl_bs_push(state, state_hash);
+    lua_newtable(L);
+    return 1;
+}
+
+static int push_map_key(void *ctx, const unsigned char *string, unsigned int len) {
+    lua_State *L = (lua_State *) ctx;
+    lua_pushlstring(L, (const char *)string, len);
+    return 1;
+}
+
+static int push_end_map(void *ctx) {
+    lua_State *L = (lua_State *) ctx;
+    yajl_bs_pop(state);
+    push_hash_or_array(L);
+    return 1;
+}
+
+static int push_start_array(void *ctx) {
+    lua_State *L = (lua_State *) ctx;
+    yajl_bs_push(state, state_array);
+    lua_newtable(L);
+    return 1;
+}
+
+static int push_end_array(void *ctx) {
+    lua_State *L = (lua_State *) ctx;
+    yajl_bs_pop(state);
+    push_hash_or_array(L);
+    return 1;
+}
+
+static yajl_callbacks callbacks = {
+    push_null,
+    push_boolean,
+    push_integer,
+    push_double,
+    NULL,
+    push_string,
+    push_start_map,
+    push_map_key,
+    push_end_map,
+    push_start_array,
+    push_end_array
+};
+
+static int parse_string(lua_State *L, const unsigned char* input, unsigned int len) {
+    yajl_parser_config cfg = { .allowComments = 1, .checkUTF8 = 0 };
+    yajl_status stat;
+    unsigned char* error = NULL;
+    char buffer[1024];
+
+    hand = yajl_alloc(&callbacks, &cfg, NULL, (void *) L);
+    yajl_bs_init(state, &(hand->alloc));
+    yajl_bs_push(state, state_begin);
+
+    stat = yajl_parse(hand, input, len);
+    if (stat == yajl_status_ok || stat == yajl_status_insufficient_data) {
+        stat = yajl_parse_complete(hand);
+    }
+
+    if (stat != yajl_status_ok) {
+        error = yajl_get_error(hand, 1, input, len);
+        strncpy(buffer, (const char *)error, 1024);
+        yajl_free_error(hand, error);
+        yajl_bs_free(state);
+        yajl_free(hand);
+        return luaL_error(L, "%s", buffer);
+    } else {
+        yajl_bs_free(state);
+        yajl_free(hand);
+    }
+
+    return 1;
+}
+
+static int parse(lua_State *L) {
+    size_t len;
+    const char *input = lua_tolstring(L, -1, &len);
+
+    if (!input)
+        return luaL_error(L, "invalid string to parse");
+
+    return parse_string(L, (const unsigned char *)input, len);
+}
+
+void json_parseString(lua_State *L, const char *input) {
+    parse_string(L, (const unsigned char *)input, strlen(input));
+}
+
+#else
+
 // json.parse("some jsons string") => table
 static int parse(lua_State *L) {
     json_parseString(L, lua_tostring(L, -1));
@@ -117,3 +274,5 @@ void json_parseString(lua_State *L, const char *input) {
         json_error("JSON Parsing Error");
     }
 }
+
+#endif
