68f944b5807981715d40280295568e853ba21e61
Corey Johnson <probablycorey@gmail.com>
2010-03-02 16:29:20 -0800

    Force simulator to close when rake run is called

--- lib/wax.h
+++ lib/wax.h
@@ -18,10 +18,3 @@ void wax_end();
 lua_State *wax_currentLuaState();
 
 void luaopen_wax(lua_State *L);
-
-static void addGlobals(lua_State *L);
-
-static int tolua(lua_State *L);
-static int toobjc(lua_State *L);
-static int exitApp(lua_State *L);
-static int objcDebug(lua_State *L);
