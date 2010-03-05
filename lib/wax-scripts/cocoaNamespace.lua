ec7a1ab807b71dd8024aa9a4c925ead441fcd3f2
Corey Johnson <probablycorey@gmail.com>
2010-03-05 13:47:24 -0800

    Objective-C Classes don't need the UI, NS prefixes anymore. Just write the class normal.

--- lib/wax-scripts/cocoaNamespace.lua
+++
@@ -1,9 +0,0 @@
--- Possibly throw this into the bridge
-_G.cocoaNamespace = function(name)
-  local metatable = {}
-  metatable.__index = function(self, key) return wax.class[self.name .. key] end
-  
-  local n = setmetatable({}, metatable)
-  n.name = name
-  _G[name] = n
-end
--- lib/wax-scripts/init.lua
+++ lib/wax-scripts/init.lua
@@ -6,15 +6,22 @@ require "wax.structs"
 require "wax.luaClass"
 require "wax.waxClass"
 require "wax.waxCallback"
-require "wax.cocoaNamespace"
 
 require "wax.bit"
-
-cocoaNamespace("UI")
-cocoaNamespace("NS")
-cocoaNamespace("MK")
-cocoaNamespace("MF")
 
 function wax.guid()
   return NS.ProcessInfo:processInfo():globallyUniqueString()
 end
+
+setmetatable(_G, {
+  __index = function(self, key)
+    local class = wax.class[key]
+    if class then self[key] = class end -- cache it for future use
+    
+    if not class and key:match("^[A-Z][A-Z][A-Z]") then -- looks like they were trying to use an objective-c obj
+      print("WARNING: No object named '" .. key .. "' found.")
+    end
+    
+    return class
+  end
+})
