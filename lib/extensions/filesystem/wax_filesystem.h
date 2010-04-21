d4dcfc3c168e5df60b8a62936d9270b11267f63d
Corey Johnson <probablycorey@gmail.com>
2010-04-21 11:35:32 -0700

    Added filesystem extension. Leaner way to get and create files from lua.

---
+++ lib/extensions/filesystem/wax_filesystem.h
@@ -0,0 +1,9 @@
+//
+//  Created by Corey Johnson on 10/5/09.
+//  Copyright 2009 Probably Interactive. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+#import "lua.h"
+
+int luaopen_wax_filesystem(lua_State *L);
