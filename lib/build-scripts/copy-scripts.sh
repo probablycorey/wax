a3f22f4acc2dea62a4ca56353a3847a15352ee50
Jeremy Collins <jeremy@perfectsensedigital.com>
2010-09-14 14:21:32 -0400

    Changed copy-scripts.sh so that it only deletes data/scripts.
    Also integrated some slight tweaks that were in the 
    main repository.

--- lib/build-scripts/copy-scripts.sh
+++ lib/build-scripts/copy-scripts.sh
@@ -10,7 +10,12 @@
 rsync -C -exclude .svn -r --delete "$PROJECT_DIR/data" "$BUILT_PRODUCTS_DIR/$CONTENTS_FOLDER_PATH" > /dev/null
 
 # copies the wax scripts over
-rsync -C -exclude .svn -r --delete "$PROJECT_DIR/wax/lib/wax-scripts/" "$BUILT_PRODUCTS_DIR/$CONTENTS_FOLDER_PATH/data/scripts/wax" > /dev/null
+if [ -d "$PROJECT_DIR/wax.framework" ]; then
+  rsync -C -exclude .svn -r --delete "$PROJECT_DIR/wax.framework/resources/wax-scripts/" "$BUILT_PRODUCTS_DIR/$CONTENTS_FOLDER_PATH/data/scripts/wax" > /dev/null
+else
+  rsync -C -exclude .svn -r --delete "$PROJECT_DIR/wax/lib/wax-scripts/" "$BUILT_PRODUCTS_DIR/$CONTENTS_FOLDER_PATH/data/scripts/wax" > /dev/null
+fi
+
 
 if [[ $CONFIGURATION = "Release" ]]; then
     LUAC=$PROJECT_DIR/wax/bin/luac
@@ -23,7 +28,7 @@ if [[ $CONFIGURATION = "Release" ]]; then
         rm $f
     done
 
-    cd $BUILT_PRODUCTS_DIR/$CONTENTS_FOLDER_PATH && zip -r resources.dat data/ && rm -rf data/
+    cd $BUILT_PRODUCTS_DIR/$CONTENTS_FOLDER_PATH && zip -r resources.dat data/ && rm -rf data/scripts
     
     touch -cm $BUILT_PRODUCTS_DIR/$CONTENTS_FOLDER_PATH
 fi
