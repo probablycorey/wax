416dec204c7f3fd3fe81f240e41f887aea8b9b47
Corey Johnson <probablycorey@gmail.com>
2010-12-28 11:04:06 -0800

    Tests updated to use new format.

--- tools/Tests/main.m
+++ tools/Tests/main.m
@@ -24,7 +24,7 @@
 int main(int argc, char *argv[]) {
     NSAutoreleasePool * pool = [[NSAutoreleasePool alloc] init];
 
-    wax_startWithExtensions(luaopen_wax_http, luaopen_wax_json, luaopen_wax_xml, nil);
+    wax_start(WAX_LUA_INIT_SCRIPT, luaopen_wax_http, luaopen_wax_json, luaopen_wax_xml, nil);
 	
     int retVal = UIApplicationMain(argc, argv, nil, [NSString stringWithUTF8String:WAX_LUA_INIT_SCRIPT]);
     [pool release];
