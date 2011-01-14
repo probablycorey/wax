ad57fb9796ef38116b83d5a1617a11e6492aed4f
Corey Johnson <probablycorey@gmail.com>
2011-01-14 09:27:44 -0800

    better var names

--- lib/stdlib/helpers/autoload.lua
+++ lib/stdlib/helpers/autoload.lua
@@ -1,10 +1,10 @@
 function wax.autoload(folders, pattern)
   for i, folder in ipairs(folders) do
-    local files = wax.filesystem.search(wax.root("scripts", folder), (pattern or "lua$"))
+    local files = wax.filesystem.search(wax.root(folder), (pattern or "lua$"))
     for i, file in ipairs(files) do
-      file = file:match(wax.root("scripts") .. "/(.*)%.lua$") 
-      file = file:gsub("/", ".")
-      require(file)
+      local requireString = file:match(wax.root() .. "/(.*)%.lua$") 
+      requireString = requireString:gsub("/", ".")
+      require(requireString)
     end
   end
 end
