5e72fd617e6fcc464b4c979588dfe9d878851bb5
Corey Johnson <cj@github.com>
2011-04-01 15:31:05 -0700

    Added timeAgoInWords method

--- lib/stdlib/helpers/time.lua
+++ lib/stdlib/helpers/time.lua
@@ -36,3 +36,26 @@ function wax.time.endOfDay(date)
   
   return calendar:dateFromComponents(newComponents)
 end
+
+function wax.time.timeAgoInWords(firstDate, secondDate)
+  local difference = (secondDate or NSDate:date()):timeIntervalSince1970() - firstDate:timeIntervalSince1970()
+  local seconds = math.abs(difference)
+  local minutes = math.floor(seconds / 60)
+  local hours = math.floor(minutes / 60)
+  local days = math.floor(hours / 24)
+  local months = math.floor(days / 30)
+
+  if minutes <= 1 then return "less than a minute"
+  elseif minutes <= 44 then return ("%d minutes"):format(minutes)
+  elseif minutes <= 89 then return "about 1 hour"
+  elseif hours <= 24 then return ("about %d hours"):format(hours)
+  elseif hours <= 47 then return "1 day"
+  elseif days <= 29 then return ("%d days"):format(days)
+  elseif days <= 59 then return "about 1 month"
+  elseif months <= 15 then return "about 1 year"
+  elseif months <= 22 then return "over 1 year"
+  elseif years < 2 then return "almost 2 years"
+  elseif minutes <= 1051199 then return "about 1 year"
+  else return ("over %d years"):format(years)
+  end
+end
