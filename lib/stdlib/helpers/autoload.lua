7b9b9ed456a167b551d89137e7ae8faaafea437c
Corey Johnson <cj@github.com>
2011-04-13 13:05:07 -0700

    autoloading pattern was stupid.

--- lib/stdlib/helpers/autoload.lua
+++ lib/stdlib/helpers/autoload.lua
@@ -1,6 +1,6 @@
-function wax.autoload(folders, pattern)
-  for i, folder in ipairs(folders) do
-    local files = wax.filesystem.search(wax.root(folder), (pattern or "lua$"))
+function wax.autoload(...)
+  for i, folder in ipairs({...}) do
+    local files = wax.filesystem.search(wax.root(folder), "lua$")
     for i, file in ipairs(files) do
       local requireString = file:match(wax.root() .. "/(.*)%.lua$") 
       requireString = requireString:gsub("/", ".")
