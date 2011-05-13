c887502cd1e71839d0b94431b845969cc96afdcd
Corey Johnson <cj@github.com>
2011-05-12 19:46:51 -0700

    xml is no longer a default extension, but filesystem is

--- xcode-template/main.m
+++ xcode-template/main.m
@@ -14,12 +14,12 @@
 #import "wax.h"
 #import "wax_http.h"
 #import "wax_json.h"
-#import "wax_xml.h"
+#import "wax_filesystem.h"
 
 int main(int argc, char *argv[]) {
     NSAutoreleasePool * pool = [[NSAutoreleasePool alloc] init];
     
-    wax_start("AppDelegate", luaopen_wax_http, luaopen_wax_json, luaopen_wax_xml, nil);
+    wax_start("AppDelegate", luaopen_wax_http, luaopen_wax_json, luaopen_wax_filesystem, nil);
     
     int retVal = UIApplicationMain(argc, argv, nil, @"AppDelegate");
     [pool release];
