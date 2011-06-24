2dbac2e47fe7550b8a4e2a796c135fafcda091cc
Corey Johnson <cj@github.com>
2011-06-24 14:04:49 -0700

    WaxClass's used to have a var called name, not it is className

--- lib/stdlib/waxClass.lua
+++ lib/stdlib/waxClass.lua
@@ -9,8 +9,8 @@ function waxInlineClass(options)
   local className = options[1]
   local superclassName = options[2]
   local class = wax.class(className, superclassName)
-  class.name = className
-  
+  class.className = className
+
   if options.protocols then
     if type(options.protocols) ~= "table" then options.protocols = {options.protocols} end
     if #options.protocols == 0 then error("\nEmpty protocol table for class " .. className .. ".\n Make sure you are defining your protocols with a string and not a variable. \n ex. protocols = {\"UITableViewDelegate\"}\n\n") end
@@ -18,25 +18,25 @@ function waxInlineClass(options)
 
   for i, protocol in ipairs(options.protocols or {}) do
     wax.class.addProtocols(class, protocol)
-  end 
+  end
 
   class._M = setmetatable({
       self = class,
     },
     {
-      __newindex = function(self, key, value) 
+      __newindex = function(self, key, value)
         class[key] = value
       end,
-    
-      __index = function(self, key) 
+
+      __index = function(self, key)
         return class[key] or _G[key]
       end,
-    
+
     }
   )
 
   _G[className] = class
   package.loaded[className] = class
-  
+
   return class
 end
