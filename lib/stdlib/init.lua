0e942259e9f6f9395a129fc34396e19a4403cd6c
Corey Johnson <cj@github.com>
2011-08-02 17:56:32 -0700

    Less warnings

--- lib/stdlib/init.lua
+++ lib/stdlib/init.lua
@@ -2,11 +2,11 @@ setmetatable(_G, {
   __index = function(self, key)
     local class = wax.class[key]
     if class then self[key] = class end -- cache it for future use
-    
-    if not class and key:match("^[A-Z][A-Z][A-Z]") then -- looks like they were trying to use an objective-c obj
+
+    if not class and key:match("^[A-Z][A-Z][A-Z][^A-Z]") then -- looks like they were trying to use an objective-c obj
       print("WARNING: No object named '" .. key .. "' found.")
     end
-    
+
     return class
   end
 })
