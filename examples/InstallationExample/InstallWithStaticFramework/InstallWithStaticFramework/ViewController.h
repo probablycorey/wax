9bb1988be37eec2c26ab79515046d7439c8d929e
junzhan.yzw <junzhan.yzw@taobao.com>
2015-11-05 15:05:35 +0800

    install with static framework

---
+++ examples/InstallationExample/InstallWithStaticFramework/InstallWithStaticFramework/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  InstallWithStaticFramework
+//
+//  Created by junzhan on 15/11/5.
+//  Copyright © 2015年 test.jz.com. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
